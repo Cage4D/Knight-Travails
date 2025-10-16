d72bdab59bdd2a96b5a2994c540b5f9f31f42fbb
Br0cod3 <thespammingbott@gmail.com>
2025-10-16 21:43:23 +0100

    remove test file

--- main.test.js
+++
@@ -1,6 +0,0 @@
-import { knightMoves } from "./main";
-
-test("Knight Moves", () => {
-    expect(knightMoves([[0, 0], [1, 2]])).toBe([[0, 0], [1, 2]])
-    expect([[[0, 0], [2, 1], [3, 3]], [[0, 0], [1, 2], [3, 3]]]).toContainEqual(knightMoves([[0, 0], [3, 3]]))
-})
